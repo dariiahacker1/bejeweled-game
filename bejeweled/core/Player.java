2e064b86f4ffc520b0f6419afa9ff1d9f5e908a5
Dariia Shumeiko <dariia.shumeiko1@gmail.com>
2025-02-15 19:45:51 +0100

    gg

--- bejeweled/core/Player.java
+++ bejeweled/core/Player.java
@@ -11,5 +11,4 @@ public class Player {
 
     public void updateScore(int points) { score += points; }
     public int getScore() { return score; }
-    public String getUsername() { return username; }
 }
